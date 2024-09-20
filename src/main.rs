bd841bc50f55f7be60ce669298af584d3993d87a
dashaw92 <dashaw92@gmail.com>
2024-09-20 11:51:25 -0400

    Added more block data encodings Marked the world as superflat to lower horizon Added support for DanWorld dimension type marker

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use dan_world::blockdata::{Axis, BisectionHalf, DanBlockData, Direction, RailShape, StairShape};
-use dan_world::DanWorld;
+use dan_world::blockdata::{
+    Axis, BisectionHalf, DanBlockData, Direction, RailShape, Side, StairShape,
+};
+use dan_world::{DanDimension, DanWorld};
 use valence::client::despawn_disconnected_clients;
 use valence::prelude::*;
 
 use valence::registry::RegistryIdx;
+use valence::spawn::IsFlat;
 use valence_sheeptag::brand::SheeptagBrandPlugin;
 use valence_sheeptag::SheeptagPlugins;
 
@@ -12,7 +15,7 @@ struct DanWorldFile(&'static str);
 
 fn main() {
     App::new()
-        .insert_resource(DanWorldFile("demo_world.dan"))
+        .insert_resource(DanWorldFile("nether_test.dan"))
         .add_plugins(SheeptagBrandPlugin)
         .add_plugins(DefaultPlugins)
         .add_plugins(SheeptagPlugins)
@@ -28,13 +31,12 @@ fn setup(
     biomes: Res<BiomeRegistry>,
     dimensions: Res<DimensionTypeRegistry>,
 ) {
-    let mut layer = LayerBundle::new(ident!("overworld"), &dimensions, &biomes, &server);
-
     let world = match DanWorld::load(world_file.0) {
         Ok(world) => world,
         Err(e) => {
             eprintln!("Failed to load DanWorld: {e:#?}");
 
+            let mut layer = LayerBundle::new(ident!("overworld"), &dimensions, &biomes, &server);
             for z in -5..5 {
                 for x in -5..5 {
                     layer.chunk.insert_chunk([x, z], UnloadedChunk::new());
@@ -52,6 +54,13 @@ fn setup(
         }
     };
 
+    let dim = match world.dimension {
+        DanDimension::Overworld => ident!("overworld"),
+        DanDimension::Nether => ident!("the_nether"),
+        DanDimension::End => ident!("the_end"),
+    };
+
+    let mut layer = LayerBundle::new(dim, &dimensions, &biomes, &server);
     place_world(world, &mut layer);
     commands.spawn(layer);
 }
@@ -63,12 +72,13 @@ fn init_clients(
             &mut VisibleChunkLayer,
             &mut Position,
             &mut GameMode,
+            &mut IsFlat,
         ),
         Added<Client>,
     >,
     layers: Query<Entity, With<ChunkLayer>>,
 ) {
-    for (mut layer_id, mut visible_chunk_layer, mut pos, mut gm) in &mut clients {
+    for (mut layer_id, mut visible_chunk_layer, mut pos, mut gm, mut flat) in &mut clients {
         let layer = layers.single();
 
         layer_id.0 = layer;
@@ -76,6 +86,7 @@ fn init_clients(
 
         pos.set([0.5, 65.0, 0.5]);
         *gm = GameMode::Creative;
+        flat.0 = true;
     }
 }
 
@@ -156,6 +167,23 @@ fn set_props(mut state: BlockState, data: &[DanBlockData]) -> BlockState {
         };
 
         let (prop, val) = to_prop(d);
+
+        if prop == PropName::Half {
+            //Handles minecraft using two different names for the property. Valence
+            //felt the need to encode this oddity into their API, so while both doors (as an example)
+            //and stairs both use the `Half` property, Minecraft opted to name the value either "top" or "upper"
+            //and "bottom" or "lower", depending on the block. Rather than attempt to match against all possibilities (error-prone),
+            //I'll just brute force it and set both values.
+            //When set is called on BlockState, it checks the PropValue for validity, returning self unmodified if the value
+            //is not applicable, resolving the issue.
+            state = match val {
+                PropValue::Top => state.set(prop, PropValue::Upper),
+                PropValue::Bottom => state.set(prop, PropValue::Lower),
+                //My code only ever attempts to use `Top` and `Bottom` for the `Half` prop,
+                //so this branch will never be hit.
+                _ => unreachable!(),
+            };
+        }
         state = state.set(prop, val);
     }
 
@@ -184,10 +212,19 @@ fn to_prop(data: &DanBlockData) -> (PropName, PropValue) {
         ),
         &DanBlockData::Direction(ref dir) => (PropName::Facing, dir_to_prop_val(dir)),
         &DanBlockData::Waterlogged(ref wl) => (PropName::Waterlogged, bool_to_prop_val(*wl)),
-        &DanBlockData::Rotation(ref rot) => (PropName::Rotation, dir_to_prop_val(rot)),
+        &DanBlockData::Rotation(ref rot) => (PropName::Rotation, rot_to_prop_val(rot)),
         &DanBlockData::Open(ref o) => (PropName::Open, bool_to_prop_val(*o)),
         &DanBlockData::RailShape(ref r) => (PropName::Shape, rail_to_prop(r)),
         &DanBlockData::StairShape(ref s) => (PropName::Shape, stair_to_shape(s)),
+        &DanBlockData::Attached(ref a) => (PropName::Attached, bool_to_prop_val(*a)),
+        &DanBlockData::Hinge(ref side) => (
+            PropName::Hinge,
+            match side {
+                Side::Left => PropValue::Left,
+                Side::Right => PropValue::Right,
+            },
+        ),
+        &DanBlockData::Farmland(ref m) => (PropName::Moisture, num_to_prop_val(m)),
         _ => unreachable!(),
     }
 }
@@ -230,6 +267,28 @@ fn dir_to_prop_val(dir: &Direction) -> PropValue {
         &Direction::Up => PropValue::Up,
         &Direction::Down => PropValue::Down,
         _ => PropValue::South,
+    }
+}
+
+fn rot_to_prop_val(dir: &Direction) -> PropValue {
+    match dir {
+        Direction::South => PropValue::_0,
+        Direction::SouthSouthWest => PropValue::_1,
+        Direction::SouthWest => PropValue::_2,
+        Direction::WestSouthWest => PropValue::_3,
+        Direction::West => PropValue::_4,
+        Direction::WestNorthWest => PropValue::_5,
+        Direction::NorthWest => PropValue::_6,
+        Direction::NorthNorthWest => PropValue::_7,
+        Direction::North => PropValue::_8,
+        Direction::NorthNorthEast => PropValue::_9,
+        Direction::NorthEast => PropValue::_10,
+        Direction::EastNorthEast => PropValue::_11,
+        Direction::East => PropValue::_12,
+        Direction::EastSouthEast => PropValue::_13,
+        Direction::SouthEast => PropValue::_14,
+        Direction::SouthSouthEast => PropValue::_15,
+        _ => unreachable!(),
     }
 }
 
